3e686d20147112734d3c37a2b9ef1632be2d19fe
walshcam <35083171+walshcam@users.noreply.github.com>
2018-06-16 20:13:06 -0400

    Add react-transition-group and AnimeJS to Main

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,15 +1,68 @@
 //==================================================================================
 // Imported Packages For The Main Page
 //==================================================================================
-
 import React, { Component } from 'react';
+import anime from 'animejs';
+import { TransitionGroup, Transition } from 'react-transition-group'
+
+const data = [{
+    id: 1,
+    datax: 160,
+    className: "small square el"
+},{
+    id: 2,
+    datax: 80,
+    className: "small square el"
+},{
+    id: 3,
+    datax: 250,
+    className: "small square el"
+}]
 
 class Main extends Component {
 
+//==================================================================================
+// AnimeJS Functions
+//==================================================================================
+
+    animeRef = (project) => anime({
+        targets: project,
+        translateX: (project) => {
+            el.getAttribute(project.datax);
+        },
+        translateY: (el, i) => {
+            50 + (-50 * i);
+        },
+        scale: (el, i, l) => {
+            (l - i) + .25
+        },
+        rotate: () => {anime.random(-360, 360);},
+        duration: () => {anime.random(1200,1800);},
+        duration: () => {anime.random(800, 1600);},
+        direction: 'alternate'
+    });
+
+
+    state = {
+
+    }
+
+//==================================================================================
+// Render The Page
+//==================================================================================
     render() {
         return(
             <div>
-                This Is The Main Page
+                Navbar With My Name?
+                <TransitionGroup>
+                    {data.map( project =>
+                        <Transition
+                            key = { project.id }
+                        >
+                            
+                        </Transition>
+                    )}
+                </TransitionGroup>
             </div>
         );
     }

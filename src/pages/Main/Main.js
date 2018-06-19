c23e173094a9a14db0dab0e68f418f23098fa87b
walshcam <35083171+walshcam@users.noreply.github.com>
2018-06-19 09:47:05 -0400

    Separate data from main page. Add initial AboutMe page file

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -8,58 +8,10 @@ import { TransitionGroup, Transition } from 'react-transition-group';
 import ProjectContainer from './../../components/ProjectContainer/ProjectContainer';
 import Navbar from './../../components/Navbar/Navbar';
 import Footer from "./../../components/Footer/Footer";
-// Import Images
-import burgermenu from "../../assets/images/main/burgermenu.JPG";
-import clickygame from "../../assets/images/main/clickygame.JPG";
-import kidsfirstbank from "../../assets/images/main/kidsfirstbank.JPG";
-import opinwater from "../../assets/images/main/opinwater.JPG";
-import takeoutscrapper from "../../assets/images/main/takeoutscrapper.JPG";
-import travellog from "../../assets/images/main/travellog.JPG";
-
-
+// Import Project Data
+import data from "./projects"
 //Import Global Colors
 import '../../assets/css/color.css';
-
-//==================================================================================
-// Project Data
-//==================================================================================
-const data = [{
-    id: 1,
-    title: "OpinWater",
-    image: opinwater,
-    link: "https://opinwater.herokuapp.com/",
-    github: "https://github.com/walshcam/Opinionated-Waters"
-},{
-    id: 2,
-    title: "Clicky-Game",
-    image: clickygame,
-    link: "https://clickygame1000.herokuapp.com/",
-    github: "https://github.com/walshcam/clicky-game"
-},{
-    id: 3,
-    title: "First Kid Bank",
-    image: kidsfirstbank,
-    link: "https://kidsfirstbank.herokuapp.com/",
-    github: "https://github.com/walshcam/kidsAllowance"
-},{
-    id: 4,
-    title: "Burger Menu",
-    image: burgermenu,
-    link: "https://fathomless-shore-70165.herokuapp.com/",
-    github: "https://github.com/walshcam/burger"
-},{
-    id: 5,
-    title: "Travel-Log",
-    image: travellog,
-    link: "https://benjaminjvaccaro.github.io/Travel-Log/",
-    github: "https://github.com/walshcam/Travel-Log"
-},{
-    id: 6,
-    title: "The Takeout Scraper",
-    image: takeoutscrapper,
-    link: "https://thetakeoutscraper.herokuapp.com/",
-    github: "https://github.com/walshcam/MongoScraper"
-},]
 
 class Main extends Component {
 
---
+++ src/pages/Main/projects.js
@@ -0,0 +1,50 @@
+//==================================================================================
+// Project Data
+//==================================================================================
+// Import Images
+import burgermenu from "../../assets/images/main/burgermenu.JPG";
+import clickygame from "../../assets/images/main/clickygame.JPG";
+import kidsfirstbank from "../../assets/images/main/kidsfirstbank.JPG";
+import opinwater from "../../assets/images/main/opinwater.JPG";
+import takeoutscrapper from "../../assets/images/main/takeoutscrapper.JPG";
+import travellog from "../../assets/images/main/travellog.JPG";
+
+const data = [{
+    id: 1,
+    title: "OpinWater",
+    image: opinwater,
+    link: "https://opinwater.herokuapp.com/",
+    github: "https://github.com/walshcam/Opinionated-Waters"
+},{
+    id: 2,
+    title: "Clicky-Game",
+    image: clickygame,
+    link: "https://clickygame1000.herokuapp.com/",
+    github: "https://github.com/walshcam/clicky-game"
+},{
+    id: 3,
+    title: "First Kid Bank",
+    image: kidsfirstbank,
+    link: "https://kidsfirstbank.herokuapp.com/",
+    github: "https://github.com/walshcam/kidsAllowance"
+},{
+    id: 4,
+    title: "Burger Menu",
+    image: burgermenu,
+    link: "https://fathomless-shore-70165.herokuapp.com/",
+    github: "https://github.com/walshcam/burger"
+},{
+    id: 5,
+    title: "Travel-Log",
+    image: travellog,
+    link: "https://benjaminjvaccaro.github.io/Travel-Log/",
+    github: "https://github.com/walshcam/Travel-Log"
+},{
+    id: 6,
+    title: "The Takeout Scraper",
+    image: takeoutscrapper,
+    link: "https://thetakeoutscraper.herokuapp.com/",
+    github: "https://github.com/walshcam/MongoScraper"
+},]
+
+export default data;

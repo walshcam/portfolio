f4639026cb6280d8a31efac3836247193bed4311
walshcam <35083171+walshcam@users.noreply.github.com>
2018-06-20 09:28:46 -0400

    Alter Footer Information

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -18,7 +18,6 @@ const Footer = (props) => (
                     <div className = "h3"><strong>Current Page Technology</strong></div>
                     <p className = "h4"><strong>React</strong></p>
                     <p className = "mb-0"><strong>Routing: </strong>React Router</p>
-                    <p className = "mb-0"><strong>Animation: </strong>AnimeJS with React-Transition-Group</p>
                     <p className = "mb-0"><strong>CSS: </strong>Bootstrap with Radium</p>
                 </div>
             </div>

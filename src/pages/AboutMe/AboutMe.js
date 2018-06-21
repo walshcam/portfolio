fc32c8466cfbad941f13044e584413c3ad185a12
walshcam <35083171+walshcam@users.noreply.github.com>
2018-06-21 10:54:52 -0400

    The pages did not need state and were turned into stateless components.

--- src/pages/AboutMe/AboutMe.js
+++ src/pages/AboutMe/AboutMe.js
@@ -1,24 +1,16 @@
 //==================================================================================
 // Imported Packages For The Main Page
 //==================================================================================
-import React, { Component } from 'react';
-
+import React from 'react';
 // UI Elements
 import PageWrapper from './../PageWrapper';
-
 // Info Data
 import Info from './info';
 
-class AboutMe extends Component {
-
-    render() {
-        return(
-            <PageWrapper>
-                <Info/>
-            </PageWrapper>
-        )
-    }
-
-}
+const AboutMe = () => (
+    <PageWrapper>
+        <Info/>
+    </PageWrapper>
+);
 
 export default AboutMe;
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,7 +1,7 @@
 //==================================================================================
 // Imported Packages For The Main Page
 //==================================================================================
-import React, { Component } from 'react';
+import React from 'react';
 // Import UI Elements
 import ProjectContainer from './../../components/ProjectContainer/ProjectContainer';
 import PageWrapper from './../PageWrapper';
@@ -10,31 +10,19 @@ import data from "./projects"
 //Import Global Colors
 import '../../assets/css/color.css';
 
-class Main extends Component {
-
-    state = {
-        enter: true
-    }
-
-//==================================================================================
-// Render The Page
-//==================================================================================
-    render() {
-        return(
-            <PageWrapper>
-                <div className = "row">
-                    {data.map( project => (
-                        <ProjectContainer
-                            project = { project }
-                            colclass = {"col-12 col-md-6 col-lg-4"}
-                            cardclass = {"el background-slate text-white"}
-                        />
-                    ))}
-                </div>
-            </PageWrapper>
-        );
-    }
-
-}
+const Main = () => (
+    <PageWrapper>
+        <div className = "row">
+            {data.map( project => (
+                <ProjectContainer
+                    key = { project.id }
+                    project = { project }
+                    colclass = {"col-12 col-md-6 col-lg-4"}
+                    cardclass = {"el background-slate text-white"}
+                />
+            ))}
+        </div>
+    </PageWrapper>
+)
 
 export default Main;
